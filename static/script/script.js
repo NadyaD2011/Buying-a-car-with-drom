67000b1c2fa884c06dc753ca81f7aeb74f43f6fd
NadyaEvp <t_evp@mail.ru>
2024-10-29 22:30:08 +0300

    Добавляем кнопку Показать больше

--- static/script/script.js
+++ static/script/script.js
@@ -1,5 +1,15 @@
-function change() {
-    let cards_car = document.querySelectorAll('.card_car');
+const btn = document.querySelector('.button');
+const cardLetter = document.querySelectorAll('.car_card').length;
+let items = 5;
 
-    cards_car[0].classList.add('active');
-}
+btn.addEventListener('click', () => {
+    items += 5;
+    const array = Array.from(document.querySelector('cards_car').children);
+    const visItems = array.slice(0, items);
+
+    visItems.forEach(el => el.classList.add('active'));
+
+    if (visItems.length === cardLetter) {
+        btn.style.display == 'none';
+    }
+});
