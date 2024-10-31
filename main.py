dc94c096aafe63f052070bdf19b9b475b9a8431b
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-10-31 12:35:14 +0300

    исправил отступы

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ env = Environment(
 
 
 def rebuild():
-  template = env.get_template("html/blog-base_brand.html")
+  template = env.get_template("html/blog-base.html")
 
   with open("my.json", "r", encoding="utf8") as file:
     cars = json.load(file)
@@ -22,5 +22,6 @@ def rebuild():
 rebuild()
 
 server = Server()
-server.watch("html/blog-base_brand.html", rebuild)
+server.watch("html/blog-base.html", rebuild)
 server.serve(root=".")
+
